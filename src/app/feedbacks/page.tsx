30e1892a3d8bbff5012da53a4d2a2367c5463a17
Sandadi Rithvik Reddy <srreddy@EX2INDIA.LOCAL>
2024-03-19 12:27:10 +0530

    form

--- src/app/feedbacks/page.tsx
+++ src/app/feedbacks/page.tsx
@@ -1,12 +1,41 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
 
+
+
 function Feedbacks() {
+  // let feedback: string | number | readonly string[] | undefined; 
+  // const handleSubmit = (event: { preventDefault: () => void; }) => {
+  //   // event.preventDefault();
+  //   // Here you can send the feedback data to your backend or perform any other actions
+  //   console.log('Feedback submitted:', feedback);
+  //   // Optionally, you can clear the feedback field after submission
+
+  // };
+  
   return (
     <div>
       <Navbar />
-      <div>Feedback</div>
+      <div>
+      <h2 className="bold text-3xl ps-10 pt-6">Feedback Form</h2>
+      <form >
+        <div className="flex flex-col items-center">
+          <label className="block my-10" htmlFor="feedback">Your Feedback:</label>
+          <textarea className="border-2"
+            id="feedback"
+            name="feedback"
+            // value={feedback}
+            rows={5}
+            cols={50}
+            
+            required
+          />
+        <button className="border-2 mt-4">Submit</button>
+        </div>
+      </form>
     </div>
+    </div>
+    
   );
 }
 
